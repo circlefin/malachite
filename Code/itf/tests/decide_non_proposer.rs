3474f62e449e28442c32495b5c0efb29a1bbde7a
Romain Ruetschi <romain@informal.systems>
2023-10-25 17:38:12 +0200

    Use fixtures from `Specs/Quint` directory

--- Code/itf/tests/decide_non_proposer.rs
+++ Code/itf/tests/decide_non_proposer.rs
@@ -1,9 +1,17 @@
 use malachite_itf::ItfState;
 
-const FIXTURE_JSON: &str = include_str!("../fixtures/0DecideNonProposerTest.itf.json");
+const FIXTURES: &[&str] = &["DecideNonProposerTest0.itf.json"];
 
 #[test]
-fn parse_fixture() {
-    let state = itf::trace_from_str::<ItfState>(FIXTURE_JSON).unwrap();
-    dbg!(state);
+fn parse_fixtures() {
+    for fixture in FIXTURES {
+        println!("Parsing '{fixture}'");
+
+        let path = format!("{}/tests/fixtures/{}", env!("CARGO_MANIFEST_DIR"), fixture);
+
+        let json = std::fs::read_to_string(&path).unwrap();
+        let state = itf::trace_from_str::<ItfState>(&json).unwrap();
+
+        dbg!(state);
+    }
 }
