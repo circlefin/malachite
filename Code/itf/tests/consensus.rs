30fdde0703d0732c25f1f1bcb17b34f2a731198b
Romain Ruetschi <romain@informal.systems>
2023-11-10 14:34:06 +0100

    Cleanup

---
+++ Code/itf/tests/consensus.rs
@@ -0,0 +1,20 @@
+use malachite_itf::consensus::State;
+
+#[test]
+fn parse_fixtures() {
+    let folder = format!("{}/tests/fixtures/consensus", env!("CARGO_MANIFEST_DIR"));
+
+    let fixtures = std::fs::read_dir(folder)
+        .unwrap()
+        .map(|entry| entry.unwrap().path())
+        .collect::<Vec<_>>();
+
+    for fixture in fixtures {
+        println!("Parsing '{}'", fixture.display());
+
+        let json = std::fs::read_to_string(&fixture).unwrap();
+        let state = itf::trace_from_str::<State>(&json).unwrap();
+
+        dbg!(state);
+    }
+}
