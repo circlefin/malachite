f82a8a5a28636c85bfdb98b658813b08e3008321
Ranadeep Biswas <mail@rnbguy.at>
2023-11-22 12:14:12 +0100

    use Trace::run_on

--- Code/itf/tests/votekeeper.rs
+++ Code/itf/tests/votekeeper.rs
@@ -2,7 +2,6 @@ use std::path::PathBuf;
 
 use malachite_itf::votekeeper::State;
 
-use itf::Runner as ItfRunner;
 use rstest::rstest;
 
 #[path = "votekeeper/runner.rs"]
@@ -15,14 +14,11 @@ use runner::{vote_keeper_runner, VoteKeeperRunner};
 #[rstest]
 fn test_itf(
     #[files("tests/fixtures/votekeeper/*.itf.json")] json_fixture: PathBuf,
-    mut vote_keeper_runner: VoteKeeperRunner,
+    vote_keeper_runner: VoteKeeperRunner,
 ) {
     println!("Parsing {json_fixture:?}");
 
     let json = std::fs::read_to_string(&json_fixture).unwrap();
     let trace = itf::trace_from_str::<State>(&json).unwrap();
-
-    vote_keeper_runner
-        .test(trace.states.into_iter().map(|s| s.value))
-        .unwrap();
+    trace.run_on(vote_keeper_runner).unwrap();
 }
