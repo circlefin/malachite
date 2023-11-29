1d6f7e89d43e87bb82158e2f66f4d7cc7161f83b
Romain Ruetschi <romain@informal.systems>
2023-11-29 10:59:18 +0100

    Cleanup doc comments

--- Code/round/src/events.rs
+++ Code/round/src/events.rs
@@ -5,21 +5,71 @@ pub enum Event<Ctx>
 where
     Ctx: Context,
 {
-    NewRound,                 // Start a new round, either as proposer or not. L14/L20
-    ProposeValue(Ctx::Value), // Propose a value.L14
-    Proposal(Ctx::Proposal),  // Receive a proposal. L22 + L23 (valid)
-    InvalidProposal,          // Receive an invalid proposal. L26 + L32 (invalid)
-    ProposalAndPolkaPrevious(Ctx::Proposal), // Received a proposal and a polka value from a previous round. L28 + L29 (valid)
-    InvalidProposalAndPolkaPrevious(Ctx::Proposal), // Received a proposal and a polka value from a previous round. L28 + L29 (invalid)
-    PolkaValue(ValueId<Ctx>),                       // Receive +2/3 prevotes for valueId. L44
-    PolkaAny,                                       // Receive +2/3 prevotes for anything. L34
-    PolkaNil,                                       // Receive +2/3 prevotes for nil. L44
-    ProposalAndPolkaCurrent(Ctx::Proposal), // Receive +2/3 prevotes for Value in current round. L36
-    PrecommitAny,                           // Receive +2/3 precommits for anything. L47
-    ProposalAndPrecommitValue(Ctx::Proposal), // Receive +2/3 precommits for Value. L49
-    PrecommitValue(ValueId<Ctx>),           // Receive +2/3 precommits for ValueId. L51
-    SkipRound(Round), // Receive +1/3 messages from a higher round. OneCorrectProcessInHigherRound, L55
-    TimeoutPropose,   // Timeout waiting for proposal. L57
-    TimeoutPrevote,   // Timeout waiting for prevotes. L61
-    TimeoutPrecommit, // Timeout waiting for precommits. L65
+    /// Start a new round, either as proposer or not.
+    /// L14/L20
+    NewRound,
+
+    /// Propose a value.
+    /// L14
+    ProposeValue(Ctx::Value),
+
+    /// Receive a proposal.
+    /// L22 + L23 (valid)
+    Proposal(Ctx::Proposal),
+
+    /// Receive an invalid proposal.
+    /// L26 + L32 (invalid)
+    InvalidProposal,
+
+    /// Received a proposal and a polka value from a previous round.
+    /// L28 + L29 (valid)
+    ProposalAndPolkaPrevious(Ctx::Proposal),
+
+    /// Received a proposal and a polka value from a previous round.
+    /// L28 + L29 (invalid)
+    InvalidProposalAndPolkaPrevious(Ctx::Proposal),
+
+    /// Receive +2/3 prevotes for a value.
+    /// L44
+    PolkaValue(ValueId<Ctx>),
+
+    /// Receive +2/3 prevotes for anything.
+    /// L34
+    PolkaAny,
+
+    /// Receive +2/3 prevotes for nil.
+    /// L44
+    PolkaNil,
+
+    /// Receive +2/3 prevotes for a value in current round.
+    /// L36
+    ProposalAndPolkaCurrent(Ctx::Proposal),
+
+    /// Receive +2/3 precommits for anything.
+    /// L47
+    PrecommitAny,
+
+    /// Receive +2/3 precommits for a value.
+    /// L49
+    ProposalAndPrecommitValue(Ctx::Proposal),
+
+    /// Receive +2/3 precommits for a value.
+    /// L51
+    PrecommitValue(ValueId<Ctx>),
+
+    /// Receive +1/3 messages from a higher round. OneCorrectProcessInHigherRound.
+    /// L55
+    SkipRound(Round),
+
+    /// Timeout waiting for proposal.
+    /// L57
+    TimeoutPropose,
+
+    /// Timeout waiting for prevotes.
+    /// L61
+    TimeoutPrevote,
+
+    /// Timeout waiting for precommits.
+    /// L65
+    TimeoutPrecommit,
 }
