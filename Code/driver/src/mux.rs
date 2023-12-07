6f7319c71d3e46420de976c2421bc5abfa0ee93f
Anca Zamfir <zamfiranca@gmail.com>
2023-12-07 13:05:23 +0100

    Sketch for mixer reorg

--- Code/driver/src/mux.rs
+++ Code/driver/src/mux.rs
@@ -1,50 +1,98 @@
+use crate::proposals::Proposals;
+use crate::Validity;
+use malachite_common::ValueId;
 use malachite_common::{Context, Proposal, Round, Value, VoteType};
 use malachite_round::input::Input as RoundInput;
+use malachite_round::state::State as RoundState;
 use malachite_round::state::Step;
+use malachite_vote::keeper::Output as VoteKeeperOutput;
 use malachite_vote::keeper::VoteKeeper;
 use malachite_vote::Threshold;
 
-use crate::proposals::Proposals;
-
 pub fn multiplex_proposal<Ctx>(
-    input: RoundInput<Ctx>,
-    input_round: Round,
-    proposals: &Proposals<Ctx>,
-) -> RoundInput<Ctx>
+    round_state: &RoundState<Ctx>,
+    votekeeper: &VoteKeeper<Ctx>,
+    proposal: Ctx::Proposal,
+    validity: Validity,
+) -> Option<RoundInput<Ctx>>
 where
     Ctx: Context,
 {
-    match input {
-        // Check if we have a proposal for the input round,
-        // if so, send `ProposalAndPolkaCurrent` instead of `PolkaAny`
-        // to the state machine.
-        RoundInput::PolkaValue(value_id) => {
-            let proposal = proposals.find(&value_id, |p| p.round() == input_round);
-
-            if let Some(proposal) = proposal {
-                assert_eq!(proposal.value().id(), value_id);
-                RoundInput::ProposalAndPolkaCurrent(proposal.clone())
-            } else {
-                RoundInput::PolkaAny
-            }
-        }
-
-        // Check if we have a proposal for the input round,
-        // if so, send `ProposalAndPrecommitValue` instead of `PrecommitAny`.
-        RoundInput::PrecommitValue(value_id) => {
-            let proposal = proposals.find(&value_id, |p| p.round() == input_round);
-
-            if let Some(proposal) = proposal {
-                assert_eq!(proposal.value().id(), value_id);
-                RoundInput::ProposalAndPrecommitValue(proposal.clone())
-            } else {
-                RoundInput::PrecommitAny
-            }
-        }
-
-        // Otherwise, just pass the input through.
-        _ => input,
+    // Check that there is an ongoing round
+    if round_state.round == Round::Nil {
+        return None;
     }
+
+    // Check that the proposal is for the current height
+    if round_state.height != proposal.height() {
+        return None;
+    }
+
+    let polka_for_pol = votekeeper.is_threshold_met(
+        &proposal.pol_round(),
+        VoteType::Prevote,
+        Threshold::Value(proposal.value().id()),
+    );
+
+    let polka_previous = proposal.pol_round().is_defined()
+        && polka_for_pol
+        && proposal.pol_round() < round_state.round;
+
+    // Handle invalid proposal
+    if !validity.is_valid() {
+        if round_state.step == Step::Propose {
+            if proposal.pol_round().is_nil() {
+                // L26
+                return Some(RoundInput::InvalidProposal);
+            } else if polka_previous {
+                // L32
+                return Some(RoundInput::InvalidProposalAndPolkaPrevious(
+                    proposal.clone(),
+                ));
+            } else {
+                return None;
+            }
+        } else {
+            return None;
+        }
+    }
+
+    // We have a valid proposal.
+    // L49
+    // TODO - check if not already decided
+    if votekeeper.is_threshold_met(
+        &proposal.round(),
+        VoteType::Precommit,
+        Threshold::Value(proposal.value().id()),
+    ) {
+        return Some(RoundInput::ProposalAndPrecommitValue(proposal.clone()));
+    }
+
+    // If the proposal is for a different round, drop the proposal
+    if round_state.round != proposal.round() {
+        return None;
+    }
+
+    let polka_for_current = votekeeper.is_threshold_met(
+        &proposal.round(),
+        VoteType::Prevote,
+        Threshold::Value(proposal.value().id()),
+    );
+
+    let polka_current = polka_for_current && round_state.step >= Step::Prevote;
+
+    // L36
+    if polka_current {
+        return Some(RoundInput::ProposalAndPolkaCurrent(proposal));
+    }
+
+    // L28
+    if round_state.step == Step::Propose && polka_previous {
+        // TODO: Check proposal vr is equal to threshold vr
+        return Some(RoundInput::ProposalAndPolkaPrevious(proposal));
+    }
+
+    Some(RoundInput::Proposal(proposal))
 }
 pub fn multiplex_on_step_change<Ctx>(
     pending_step: Step,
@@ -111,4 +159,39 @@ where
     Ctx: Context,
 {
     votekeeper.is_threshold_met(&round, VoteType::Prevote, Threshold::Any)
+}
+
+pub fn multiplex_on_vote_threshold<Ctx>(
+    new_threshold: VoteKeeperOutput<ValueId<Ctx>>,
+    proposals: &Proposals<Ctx>,
+) -> Option<RoundInput<Ctx>>
+where
+    Ctx: Context,
+{
+    let proposal = proposals.all().next();
+
+    if let Some(proposal) = proposal {
+        match new_threshold {
+            VoteKeeperOutput::PolkaAny => Some(RoundInput::PolkaAny),
+            VoteKeeperOutput::PolkaNil => Some(RoundInput::PolkaNil),
+            VoteKeeperOutput::PolkaValue(v) => {
+                if v == proposal.value().id() {
+                    Some(RoundInput::ProposalAndPolkaCurrent(proposal.clone()))
+                } else {
+                    Some(RoundInput::PolkaAny)
+                }
+            }
+            VoteKeeperOutput::PrecommitAny => Some(RoundInput::PrecommitAny),
+            VoteKeeperOutput::PrecommitValue(v) => {
+                if v == proposal.value().id() {
+                    Some(RoundInput::ProposalAndPrecommitValue(proposal.clone()))
+                } else {
+                    Some(RoundInput::PrecommitAny)
+                }
+            }
+            VoteKeeperOutput::SkipRound(r) => Some(RoundInput::SkipRound(r)),
+        }
+    } else {
+        None
+    }
 }
