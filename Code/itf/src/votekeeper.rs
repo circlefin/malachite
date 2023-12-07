808b0281dabbff71f2840be3ade76d6e06f8f81c
Romain Ruetschi <romain@informal.systems>
2023-12-07 21:34:12 +0100

    Use `itf` native support for sum types (#111)

--- Code/itf/src/votekeeper.rs
+++ Code/itf/src/votekeeper.rs
@@ -3,113 +3,34 @@ use std::collections::{HashMap, HashSet};
 
 use serde::Deserialize;
 
-use crate::types::{
-    Address, EmptyObject, Height, NonNilValue, Round, SerdeValue, SerdeVoteType, Value, VoteType,
-    Weight,
-};
+use crate::types::{Address, Height, NonNilValue, Round, Value, VoteType, Weight};
 
 #[derive(Clone, Debug, PartialEq, Eq, Deserialize)]
-#[serde(untagged)]
-pub enum WeightedVoteValues {
-    #[serde(with = "As::<(Same, Integer, Integer)>")]
-    WV((Vote, Weight, Round)),
-    NoWeightedVote(EmptyObject),
-}
-
-#[derive(Clone, Debug, PartialEq, Eq, Deserialize)]
-pub struct WeightedVoteTag {
-    pub tag: String,
-    pub value: WeightedVoteValues,
-}
-
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[serde(tag = "tag", content = "value")]
 pub enum WeightedVote {
     NoWeightedVote,
+
+    #[serde(with = "As::<(Same, Integer, Integer)>")]
     WV(Vote, Weight, Round),
 }
 
-impl TryFrom<WeightedVoteTag> for WeightedVote {
-    type Error = String;
-    fn try_from(v: WeightedVoteTag) -> Result<Self, Self::Error> {
-        match v.tag.as_str() {
-            "NoWeightedVote" => Ok(WeightedVote::NoWeightedVote),
-            "WV" => match v.value {
-                WeightedVoteValues::WV((v, w, r)) => Ok(WeightedVote::WV(v, w, r)),
-                WeightedVoteValues::NoWeightedVote(_) => todo!(),
-            },
-            _ => todo!(), // error
-        }
-    }
-}
-
-pub type SerdeWeightedVote = serde_with::TryFromInto<WeightedVoteTag>;
-
-#[derive(Clone, Debug, PartialEq, Eq, Deserialize, Hash)]
-#[serde(untagged)]
-pub enum VoteKeeperOutputValues {
-    None(EmptyObject),
-    #[serde(with = "As::<Integer>")]
-    Round(Round),
-    #[serde(with = "As::<(Integer, Same)>")]
-    RoundValue((Round, NonNilValue)),
-}
-
-#[derive(Clone, Debug, PartialEq, Eq, Deserialize, Hash)]
-pub struct VoteKeeperOutputTag {
-    pub tag: String,
-    pub value: VoteKeeperOutputValues,
-}
-
-#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash, Deserialize)]
+#[serde(tag = "tag", content = "value")]
 pub enum VoteKeeperOutput {
     NoVKOutput,
+    #[serde(with = "As::<Integer>")]
     PolkaAnyVKOutput(Round),
+    #[serde(with = "As::<Integer>")]
     PolkaNilVKOutput(Round),
+    #[serde(with = "As::<(Integer, Same)>")]
     PolkaValueVKOutput(Round, NonNilValue),
+    #[serde(with = "As::<Integer>")]
     PrecommitAnyVKOutput(Round),
+    #[serde(with = "As::<(Integer, Same)>")]
     PrecommitValueVKOutput(Round, NonNilValue),
+    #[serde(with = "As::<Integer>")]
     SkipVKOutput(Round),
 }
-
-impl TryFrom<VoteKeeperOutputTag> for VoteKeeperOutput {
-    type Error = String;
-    fn try_from(v: VoteKeeperOutputTag) -> Result<Self, Self::Error> {
-        match v.tag.as_str() {
-            "NoVKOutput" => Ok(VoteKeeperOutput::NoVKOutput),
-            "PolkaAnyVKOutput" => match v.value {
-                VoteKeeperOutputValues::Round(r) => Ok(VoteKeeperOutput::PolkaAnyVKOutput(r)),
-                _ => todo!(), // error
-            },
-            "PolkaNilVKOutput" => match v.value {
-                VoteKeeperOutputValues::Round(r) => Ok(VoteKeeperOutput::PolkaNilVKOutput(r)),
-                _ => todo!(), // error
-            },
-            "PolkaValueVKOutput" => match v.value {
-                VoteKeeperOutputValues::RoundValue((r, v)) => {
-                    Ok(VoteKeeperOutput::PolkaValueVKOutput(r, v))
-                }
-                _ => todo!(), // error
-            },
-            "PrecommitAnyVKOutput" => match v.value {
-                VoteKeeperOutputValues::Round(r) => Ok(VoteKeeperOutput::PrecommitAnyVKOutput(r)),
-                _ => todo!(), // error
-            },
-            "PrecommitValueVKOutput" => match v.value {
-                VoteKeeperOutputValues::RoundValue((r, v)) => {
-                    Ok(VoteKeeperOutput::PrecommitValueVKOutput(r, v))
-                }
-                _ => todo!(), // error
-            },
-            "SkipVKOutput" => match v.value {
-                VoteKeeperOutputValues::Round(r) => Ok(VoteKeeperOutput::SkipVKOutput(r)),
-                _ => todo!(), // error
-            },
-            _ => todo!(), // error
-        }
-    }
-}
-
-pub type SerdeVoteKeeperOutput = serde_with::TryFromInto<VoteKeeperOutputTag>;
 
 #[derive(Clone, Debug, PartialEq, Eq, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -125,13 +46,11 @@ pub struct Bookkeeper {
 #[derive(Clone, Debug, PartialEq, Eq, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Vote {
-    #[serde(with = "As::<SerdeVoteType>")]
     pub vote_type: VoteType,
     #[serde(with = "As::<Integer>")]
     pub height: Height,
     #[serde(with = "As::<Integer>")]
     pub round: Round,
-    #[serde(with = "As::<SerdeValue>")]
     pub value_id: Value,
     pub src_address: Address,
 }
@@ -145,7 +64,6 @@ pub struct RoundVotes {
     pub round: Round,
     pub prevotes: VoteCount,
     pub precommits: VoteCount,
-    #[serde(with = "As::<HashSet<SerdeVoteKeeperOutput>>")]
     pub emitted_outputs: HashSet<VoteKeeperOutput>,
     #[serde(with = "As::<HashMap<Same, Integer>>")]
     pub votes_addresses_weights: HashMap<Address, Weight>,
@@ -156,7 +74,7 @@ pub struct RoundVotes {
 pub struct VoteCount {
     #[serde(with = "As::<Integer>")]
     pub total_weight: Weight,
-    #[serde(with = "As::<HashMap<SerdeValue, Integer>>")]
+    #[serde(with = "As::<HashMap<Same, Integer>>")]
     pub values_weights: HashMap<Value, Weight>,
     pub votes_addresses: HashSet<Address>,
 }
@@ -165,8 +83,6 @@ pub struct VoteCount {
 #[serde(rename_all = "camelCase")]
 pub struct State {
     pub bookkeeper: Bookkeeper,
-    #[serde(with = "As::<SerdeVoteKeeperOutput>")]
     pub last_emitted: VoteKeeperOutput,
-    #[serde(with = "As::<SerdeWeightedVote>")]
     pub weighted_vote: WeightedVote,
 }
