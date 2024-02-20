abb62cc5a9d2831e63b2c04e206716938e581921
Romain Ruetschi <romain@informal.systems>
2024-02-20 08:01:12 +0100

    Implement `Network` for `broadcast::Handle`

--- code/node/src/network.rs
+++ code/node/src/network.rs
@@ -1,3 +1,5 @@
+use core::fmt;
+
 use malachite_common::Context;
 
 pub mod broadcast;
@@ -5,9 +7,18 @@ mod msg;
 
 pub use self::msg::Msg;
 
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct PeerId(String);
+
+impl fmt::Display for PeerId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Display::fmt(&self.0, f)
+    }
+}
+
 #[allow(async_fn_in_trait)]
 pub trait Network<Ctx: Context> {
-    async fn recv(&mut self) -> Option<Msg<Ctx>>;
+    async fn recv(&mut self) -> Option<(PeerId, Msg<Ctx>)>;
     async fn broadcast(&mut self, msg: Msg<Ctx>);
 
     async fn broadcast_vote(&mut self, vote: Ctx::Vote) {
