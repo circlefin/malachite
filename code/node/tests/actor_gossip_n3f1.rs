9294c595316be12d7347758d118b1815971ba7ea
Romain Ruetschi <romain@informal.systems>
2024-03-06 16:25:06 +0100

    Discard all gossip events

--- code/node/tests/actor_gossip_n3f1.rs
+++ code/node/tests/actor_gossip_n3f1.rs
@@ -6,7 +6,7 @@ use util::*;
 pub async fn discard_gossip_event_fail() {
     let nodes = Test::new(
         [
-            TestNode::faulty(10, vec![Fault::DiscardGossipEvent(0.5)]),
+            TestNode::faulty(10, vec![Fault::DiscardGossipEvent(1.0)]),
             TestNode::faulty(5, vec![]),
             TestNode::faulty(5, vec![]),
         ],
@@ -20,7 +20,7 @@ pub async fn discard_gossip_event_fail() {
 pub async fn discard_gossip_event_ok() {
     let nodes = Test::new(
         [
-            TestNode::faulty(5, vec![Fault::DiscardGossipEvent(0.5)]),
+            TestNode::faulty(5, vec![Fault::DiscardGossipEvent(1.0)]),
             TestNode::faulty(15, vec![]),
             TestNode::faulty(15, vec![]),
         ],
