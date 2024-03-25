77e481d04f450d7b5c05c1b54fa0d00af781596d
Romain Ruetschi <romain@informal.systems>
2024-03-25 18:55:36 +0100

    Cleanup

--- code/node/src/value.rs
+++ code/node/src/value.rs
@@ -1,24 +1,19 @@
 use std::marker::PhantomData;
 use std::time::Instant;
 
+use async_trait::async_trait;
 use derive_where::derive_where;
 
-use futures::future::BoxFuture;
 use malachite_common::Context;
 
-#[allow(async_fn_in_trait)]
+#[async_trait]
 pub trait ValueBuilder<Ctx: Context>: Send + Sync + 'static {
-    fn build_proposal(
-        &self,
-        height: Ctx::Height,
-        deadline: Instant,
-    ) -> BoxFuture<Option<Ctx::Value>>;
+    async fn build_proposal(&self, height: Ctx::Height, deadline: Instant) -> Option<Ctx::Value>;
 }
 
 pub mod test {
     use super::*;
 
-    use futures::FutureExt;
     use malachite_test::{Height, TestContext, Value};
 
     #[derive_where(Default)]
@@ -26,17 +21,15 @@ pub mod test {
         _phantom: PhantomData<Ctx>,
     }
 
+    #[async_trait]
     impl ValueBuilder<TestContext> for TestValueBuilder<TestContext> {
-        fn build_proposal(&self, height: Height, deadline: Instant) -> BoxFuture<Option<Value>> {
-            async move {
-                let diff = deadline.duration_since(Instant::now());
-                let wait = diff / 2;
+        async fn build_proposal(&self, height: Height, deadline: Instant) -> Option<Value> {
+            let diff = deadline.duration_since(Instant::now());
+            let wait = diff / 2;
 
-                tokio::time::sleep(wait).await;
+            tokio::time::sleep(wait).await;
 
-                Some(Value::new(40 + height.as_u64()))
-            }
-            .boxed()
+            Some(Value::new(40 + height.as_u64()))
         }
     }
 }
