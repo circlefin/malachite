d4b9f354836a32a217d01d521dc14418b32e887e
Romain Ruetschi <romain@informal.systems>
2024-06-04 11:45:21 +0200

    Add `testnet` command, and clean up CLI (#205)
    * removed index parameter, added testnet command, simplified init command
    
    * Update help message
    
    * Remove `--config`, `--genesis` and `--private-key` options
    
    * Unpretty private key bytes
    
    * Log output in `testnet` command
    
    * Use CometBFT-compatible format for `priv_validator_key.json` file
    
    * Add test for `testnet` command
    
    ---------
    
    Co-authored-by: Greg Szabo <greg@philosobear.com>

--- code/cli/src/args.rs
+++ code/cli/src/args.rs
@@ -1,16 +1,13 @@
 //! Node command-line interface configuration
 //!
-//! The node CLI reads configuration from the configuration file provided with the
-//! `--config` parameter. Some configuration parameters can be overridden on the command-line.
+//! The node CLI reads configuration from the configuration files found in the directory
+//! provided with the `--home` global parameter.
 //!
 //! The command-line parameters are stored in the `Args` structure.
 //! `clap` parses the command-line parameters into this structure.
-//!
 
 use std::path::{Path, PathBuf};
 
-use base64::prelude::BASE64_STANDARD;
-use base64::Engine;
 use clap::{Parser, Subcommand};
 use color_eyre::eyre::{eyre, Context, Result};
 use directories::BaseDirs;
@@ -20,6 +17,7 @@ use malachite_node::config::Config;
 use malachite_test::{PrivateKey, ValidatorSet};
 
 use crate::logging::DebugSection;
+use crate::priv_key::PrivValidatorKey;
 
 const APP_FOLDER: &str = ".malachite";
 const CONFIG_FILE: &str = "config.toml";
@@ -29,36 +27,13 @@ const PRIV_VALIDATOR_KEY_FILE: &str = "priv_validator_key.json";
 #[derive(Parser, Clone, Debug, Default)]
 #[command(version, about, long_about = None)]
 pub struct Args {
-    /// Config file path
-    #[arg(long, value_name = "HOME_DIR")]
+    /// Home directory for Malachite (default: `~/.malachite`)
+    #[arg(long, global = true, value_name = "HOME_DIR")]
     pub home: Option<PathBuf>,
 
-    /// Config file path
-    #[arg(short, long, value_name = "CONFIG_FILE")]
-    pub config: Option<PathBuf>,
-
-    /// Genesis file path
-    #[arg(short, long, value_name = "GENESIS_FILE")]
-    pub genesis: Option<PathBuf>,
-
-    /// Base64-encoded private key
     #[clap(
         long,
-        default_value = "",
-        hide_default_value = true,
-        value_name = "BASE64_STRING",
-        env = "PRIVATE_KEY",
-        value_parser = | s: & str | BASE64_STANDARD.decode(s)
-    )]
-    pub private_key: std::vec::Vec<u8>, // Keep the fully qualified path for Vec<u8> or else clap will not be able to parse it: https://github.com/clap-rs/clap/issues/4481.
-
-    /// Validator index only for the init command
-    #[clap(short, long, value_name = "INDEX", env = "INDEX")]
-    pub index: Option<usize>,
-
-    #[clap(
-        short,
-        long = "debug",
+        global = true,
         help = "Enable debug output for the given comma-separated sections",
         value_enum,
         value_delimiter = ','
@@ -71,12 +46,26 @@ pub struct Args {
 
 #[derive(Subcommand, Clone, Debug, Default, PartialEq)]
 pub enum Commands {
-    /// Initialize configuration
-    ///
-    Init,
     /// Start node
     #[default]
     Start,
+
+    /// Initialize configuration
+    Init,
+
+    /// Generate testnet configuration
+    Testnet(TestnetArgs),
+}
+
+#[derive(Parser, Debug, Clone, PartialEq)]
+pub struct TestnetArgs {
+    /// Number of validator nodes in the testnet
+    #[clap(short, long)]
+    pub nodes: usize,
+
+    /// Generate deterministic private keys for reproducibility
+    #[clap(short, long)]
+    pub deterministic: bool,
 }
 
 impl Args {
@@ -105,19 +94,13 @@ impl Args {
     /// get_config_file_path returns the configuration file path based on the command-ine arguments
     /// and the configuration folder.
     pub fn get_config_file_path(&self) -> Result<PathBuf> {
-        Ok(match &self.config {
-            Some(path) => path.clone(),
-            None => self.get_config_dir()?.join(CONFIG_FILE),
-        })
+        Ok(self.get_config_dir()?.join(CONFIG_FILE))
     }
 
     /// get_genesis_file_path returns the genesis file path based on the command-line arguments and
     /// the configuration folder.
     pub fn get_genesis_file_path(&self) -> Result<PathBuf> {
-        Ok(match &self.genesis {
-            Some(path) => path.clone(),
-            None => self.get_config_dir()?.join(GENESIS_FILE),
-        })
+        Ok(self.get_config_dir()?.join(GENESIS_FILE))
     }
 
     /// get_priv_validator_key_file_path returns the private validator key file path based on the
@@ -143,18 +126,10 @@ impl Args {
     /// load_private_key returns the private key either from the command-line parameter or
     /// from the priv_validator_key.json file.
     pub fn load_private_key(&self) -> Result<PrivateKey> {
-        if self.private_key.is_empty()
-            || self.private_key == vec![0u8; 32]
-            || self.private_key.len() < 32
-        {
-            let priv_key_file = self.get_priv_validator_key_file_path()?;
-            info!("Loading private key from {:?}", priv_key_file.display());
-            load_json_file(&priv_key_file)
-        } else {
-            let mut key: [u8; 32] = [0; 32];
-            key.copy_from_slice(&self.private_key);
-            Ok(PrivateKey::from(key))
-        }
+        let priv_key_file = self.get_priv_validator_key_file_path()?;
+        info!("Loading private key from {:?}", priv_key_file.display());
+        let priv_validator_key: PrivValidatorKey = load_json_file(&priv_key_file)?;
+        Ok(priv_validator_key.private_key)
     }
 }
 
@@ -193,22 +168,6 @@ mod tests {
         let args = Args::parse_from(["test", "start"]);
         assert_eq!(args.debug, vec![]);
         assert_eq!(args.command, Commands::Start);
-
-        let args = Args::parse_from([
-            "test",
-            "--config",
-            "myconfig.toml",
-            "--genesis",
-            "mygenesis.json",
-            "--private-key",
-            "c2VjcmV0",
-            "init",
-        ]);
-        assert_eq!(args.config, Some(PathBuf::from("myconfig.toml")));
-        assert_eq!(args.genesis, Some(PathBuf::from("mygenesis.json")));
-        assert_eq!(args.private_key, b"secret");
-        assert!(args.get_home_dir().is_ok());
-        assert!(args.get_config_dir().is_ok());
     }
 
     #[test]
@@ -228,42 +187,5 @@ mod tests {
         let mut file = tmpfile.as_file();
         writeln!(file, "{{}}").unwrap();
         assert!(load_json_file::<TestStruct>(&PathBuf::from(tmpfile.path())).is_ok());
-    }
-
-    #[test]
-    fn args_load_config() {
-        let args = Args::parse_from(["test", "--config", "../config.toml", "start"]);
-        let config = args.load_config().unwrap();
-        assert_eq!(config.moniker, "malachite");
-    }
-
-    #[test]
-    fn args_load_genesis() {
-        let args = Args::parse_from(["test", "--genesis", "../genesis.json", "start"]);
-        assert!(args.load_genesis().is_err());
-    }
-
-    #[test]
-    fn args_private_key() {
-        let args = Args::parse_from(["test", "start"]);
-        if !args.get_priv_validator_key_file_path().unwrap().exists() {
-            assert!(args.load_private_key().is_err());
-            assert!(args.private_key.is_empty());
-        }
-
-        let args = Args::parse_from(["test", "--private-key", "c2VjcmV0", "start"]);
-        if !args.get_priv_validator_key_file_path().unwrap().exists() {
-            assert!(args.load_private_key().is_err());
-        }
-
-        let args = Args::parse_from([
-            "test",
-            "--private-key",
-            "c2VjcmV0c2VjcmV0c2VjcmV0c2VjcmV0c2VjcmV0MDA=",
-            "start",
-        ]);
-        let pk = args.load_private_key().unwrap();
-
-        assert_eq!(pk.inner().as_bytes(), b"secretsecretsecretsecretsecret00");
     }
 }
