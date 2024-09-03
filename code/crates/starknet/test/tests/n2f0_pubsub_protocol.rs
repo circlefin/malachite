5cbf385dd6e7d464af46006b0f23cad43fd2ec6e
Anca Zamfir <zamfiranca@gmail.com>
2024-09-03 15:16:10 +0200

    Add test with default values

--- code/crates/starknet/test/tests/n2f0_pubsub_protocol.rs
+++ code/crates/starknet/test/tests/n2f0_pubsub_protocol.rs
@@ -14,20 +14,33 @@ async fn run_n2f0_tests(test_params: TestParams) {
 }
 
 #[tokio::test]
-pub async fn ok_flood_all_correct_nodes_custom_config() {
+pub async fn flood_default_config() {
+    let test = Test::new(
+        [
+            TestNode::correct(10),
+            TestNode::correct(10),
+        ],
+        Expected::Exactly(6),
+    );
+
+    test.run(App::Starknet).await
+}
+
+#[tokio::test]
+pub async fn flood_custom_config_1ktx() {
     let test_params = TestParams::new(PubSubProtocol::FloodSub, 1024, 1024);
     run_n2f0_tests(test_params).await
 }
 
 // This fails due to message length limit in floodsub
 #[tokio::test]
-pub async fn fail_flood_all_correct_nodes_custom_config() {
+pub async fn flood_custom_config_2ktx() {
     let test_params = TestParams::new(PubSubProtocol::FloodSub, 2048, 2048);
     run_n2f0_tests(test_params).await
 }
 
 #[tokio::test]
-pub async fn ok_gossip_all_correct_nodes_custom_config() {
+pub async fn gossip_custom_config_2ktx() {
     let test_params = TestParams::new(PubSubProtocol::GossipSub, 2048, 2048);
     run_n2f0_tests(test_params).await
 }
