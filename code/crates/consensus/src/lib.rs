5af4f4817f4500c1a864467b8c4e82b59dcb04bb
Romain Ruetschi <romain@informal.systems>
2024-09-30 13:30:20 +0200

    Hide types which do not need to be public

--- code/crates/consensus/src/lib.rs
+++ code/crates/consensus/src/lib.rs
@@ -10,18 +10,18 @@ pub use error::Error;
 mod params;
 pub use params::{Params, ThresholdParams};
 
-pub mod handle;
-
-pub mod gen;
-
 mod effect;
 pub use effect::{Effect, Resume};
 
 mod types;
 pub use types::*;
 
+pub mod gen;
+pub mod handle;
+
+mod full_proposal_keeper;
 mod macros;
 mod util;
 
-mod full_proposal_keeper;
-pub use full_proposal_keeper::FullProposalKeeper;
+#[doc(hidden)]
+pub use full_proposal_keeper::{FullProposal, FullProposalKeeper};
