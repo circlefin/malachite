c27c4b302af7aa13a9aac5a3f3f0dc4326f1bf48
Romain Ruetschi <romain@informal.systems>
2024-10-02 17:46:42 +0200

    Make macros more robust

--- code/crates/consensus/src/lib.rs
+++ code/crates/consensus/src/lib.rs
@@ -34,3 +34,7 @@ pub use handle::handle;
 // Only used internally, but needs to be exposed for tests
 #[doc(hidden)]
 pub use full_proposal::{FullProposal, FullProposalKeeper};
+
+// Used in macros
+#[doc(hidden)]
+pub use tracing;
