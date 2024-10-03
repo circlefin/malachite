fd65706e8aae1d1c9d34a40698dc61023d346d59
Romain Ruetschi <romain@informal.systems>
2024-10-03 09:53:23 +0200

    Add time to consensus trace

--- code/crates/round/src/traces/trace.rs
+++ code/crates/round/src/traces/trace.rs
@@ -1,7 +1,6 @@
-use std::time::SystemTime;
-
 use derive_where::derive_where;
 use displaydoc::Display;
+use time::OffsetDateTime;
 
 use malachite_common::{Context, Round};
 
@@ -9,20 +8,20 @@ use super::Line;
 
 #[derive_where(Clone, Debug, Eq, PartialEq)]
 #[derive(Display)]
-/// height: {height}, round: {round}, line: {line}
+/// [{time}] height: {height}, round: {round}, line: {line}
 pub struct Trace<Ctx: Context> {
+    pub time: OffsetDateTime,
     pub height: Ctx::Height,
     pub round: Round,
-    pub time: SystemTime,
     pub line: Line,
 }
 
 impl<Ctx: Context> Trace<Ctx> {
     pub fn new(height: Ctx::Height, round: Round, line: Line) -> Self {
         Self {
+            time: OffsetDateTime::now_utc(),
             height,
             round,
-            time: SystemTime::now(),
             line,
         }
     }
