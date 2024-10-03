a0e8f4d8037cc8cfee6b0320f050177ff099df36
Anca Zamfir <zamfiranca@gmail.com>
2024-10-03 12:57:22 +0200

    Clean the proposal parts on decision only, keep all at higher height.

--- code/crates/starknet/host/src/actor.rs
+++ code/crates/starknet/host/src/actor.rs
@@ -157,8 +157,6 @@ impl StarknetHost {
         round: Round,
         part: ProposalPart,
     ) -> Option<ProposedValue<MockContext>> {
-        // Prune all proposal parts for heights lower than `height - 1`
-        state.part_store.prune(height.decrement().unwrap_or(height));
         state.part_store.store(height, round, part.clone());
 
         if let ProposalPart::Transactions(txes) = &part {
@@ -367,10 +365,10 @@ impl Actor for StarknetHost {
                     }
                 }
 
-                // Prune the PartStore of all parts for heights lower than `state.height - 1`
+                // Prune the PartStore of all parts for heights lower than `state.height`
                 state
                     .part_store
-                    .prune(state.height.decrement().unwrap_or(state.height));
+                    .prune(state.height);
 
                 // Notify the mempool to remove corresponding txs
                 self.mempool.cast(MempoolMsg::Update { tx_hashes })?;
