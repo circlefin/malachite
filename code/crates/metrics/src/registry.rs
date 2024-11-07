7f2fe501aace633d63b1130613477a8f51a2686f
Romain Ruetschi <romain@informal.systems>
2024-11-07 17:55:25 +0100

    Add `moniker` label with nodes moniker to all metrics

--- code/crates/metrics/src/registry.rs
+++ code/crates/metrics/src/registry.rs
@@ -1,35 +1,60 @@
+use std::borrow::Cow;
 use std::sync::{Arc, OnceLock, RwLock};
 
 pub use prometheus_client::registry::Registry;
 
 #[derive(Clone)]
-pub struct SharedRegistry(Arc<RwLock<Registry>>);
+pub struct SharedRegistry {
+    moniker: Option<String>,
+    registry: Arc<RwLock<Registry>>,
+}
 
 impl SharedRegistry {
-    pub fn new(registry: Registry) -> Self {
-        Self(Arc::new(RwLock::new(registry)))
+    pub fn new(registry: Registry, moniker: Option<String>) -> Self {
+        Self {
+            moniker,
+            registry: Arc::new(RwLock::new(registry)),
+        }
     }
 
     pub fn global() -> &'static Self {
         global_registry()
     }
 
-    pub fn read<A>(&self, f: impl FnOnce(&Registry) -> A) -> A {
-        f(&self.0.read().expect("poisoned lock"))
+    pub fn with_moniker(&self, moniker: impl Into<String>) -> Self {
+        Self {
+            moniker: Some(moniker.into()),
+            registry: Arc::clone(&self.registry),
+        }
     }
 
-    pub fn write<A>(&self, f: impl FnOnce(&mut Registry) -> A) -> A {
-        f(&mut self.0.write().expect("poisoned lock"))
+    fn read<A>(&self, f: impl FnOnce(&Registry) -> A) -> A {
+        f(&self.registry.read().expect("poisoned lock"))
+    }
+
+    fn write<A>(&self, f: impl FnOnce(&mut Registry) -> A) -> A {
+        f(&mut self.registry.write().expect("poisoned lock"))
     }
 
     pub fn with_prefix<A>(&self, prefix: impl AsRef<str>, f: impl FnOnce(&mut Registry) -> A) -> A {
-        self.write(|reg| f(reg.sub_registry_with_prefix(prefix)))
+        if let Some(moniker) = &self.moniker {
+            self.write(|reg| {
+                f(reg
+                    .sub_registry_with_prefix(prefix)
+                    .sub_registry_with_label((
+                        Cow::Borrowed("moniker"),
+                        Cow::Owned(moniker.to_string()),
+                    )))
+            })
+        } else {
+            self.write(|reg| f(reg.sub_registry_with_prefix(prefix)))
+        }
     }
 }
 
 fn global_registry() -> &'static SharedRegistry {
     static REGISTRY: OnceLock<SharedRegistry> = OnceLock::new();
-    REGISTRY.get_or_init(|| SharedRegistry::new(Registry::default()))
+    REGISTRY.get_or_init(|| SharedRegistry::new(Registry::default(), None))
 }
 
 pub fn export<W: core::fmt::Write>(writer: &mut W) {
