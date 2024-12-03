db39aa574968f920765c785b7c9a3c4fdac529e9
Anca Zamfir <zamfiranca@gmail.com>
2024-12-03 09:44:18 -0500

    Enable blocksync_start_late

--- code/crates/starknet/test/tests/blocksync.rs
+++ code/crates/starknet/test/tests/blocksync.rs
@@ -133,37 +133,36 @@ pub async fn aggressive_pruning() {
         .await
 }
 
-// TODO: Enable this test once we can start the network without everybody being online
-// #[tokio::test]
-// pub async fn blocksync_start_late() {
-//     const HEIGHT: u64 = 5;
-//
-//     let n1 = TestNode::new(1)
-//         .voting_power(10)
-//         .start(1)
-//         .wait_until(HEIGHT * 2)
-//         .success();
-//
-//     let n2 = TestNode::new(2)
-//         .voting_power(10)
-//         .start(1)
-//         .wait_until(HEIGHT * 2)
-//         .success();
-//
-//     let n3 = TestNode::new(3)
-//         .voting_power(5)
-//         .start_after(1, Duration::from_secs(10))
-//         .wait_until(HEIGHT)
-//         .success();
-//
-//     Test::new([n1, n2, n3])
-//         .run_with_custom_config(
-//             Duration::from_secs(30),
-//             TestParams {
-//                 enable_blocksync: true,
-//                 ..Default::default()
-//             },
-//         )
-//         .await
-// }
-//
+#[tokio::test]
+pub async fn blocksync_start_late() {
+    const HEIGHT: u64 = 5;
+
+    let n1 = TestNode::new(1)
+        .vp(10)
+        .start()
+        .wait_until(HEIGHT * 2)
+        .success();
+
+    let n2 = TestNode::new(2)
+        .vp(10)
+        .start()
+        .wait_until(HEIGHT * 2)
+        .success();
+
+    let n3 = TestNode::new(3)
+        .vp(5)
+        .start_after(1, Duration::from_secs(10))
+        .wait_until(HEIGHT)
+        .success();
+
+    Test::new([n1, n2, n3])
+        .run_with_custom_config(
+            Duration::from_secs(30),
+            TestParams {
+                enable_blocksync: true,
+                ..Default::default()
+            },
+        )
+        .await
+}
+
