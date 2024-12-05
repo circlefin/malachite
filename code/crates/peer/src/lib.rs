c8d71a845e4c8bbe17ef058f148724897e536db4
Romain Ruetschi <romain@informal.systems>
2024-12-05 12:36:00 +0100

    Fix build until upstream bug is fixed
    See comments in code

--- code/crates/peer/src/lib.rs
+++ code/crates/peer/src/lib.rs
@@ -18,6 +18,8 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
+// FIXME: Make the crate no_std compatible once https://github.com/multiformats/rust-multihash/issues/375 is resolved
+
 use std::{fmt, str::FromStr};
 
 use thiserror::Error;
