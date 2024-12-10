7a2c774e2a8765de562fa94ce6a490ed518aba7f
Romain Ruetschi <romain@informal.systems>
2024-12-10 17:19:31 +0100

    Add TODO

--- code/crates/app-channel/src/lib.rs
+++ code/crates/app-channel/src/lib.rs
@@ -1,3 +1,13 @@
+// TODO: Enforce proper documentation
+// #![warn(
+//     missing_docs,
+//     clippy::empty_docs,
+//     clippy::missing_errors_doc,
+//     rustdoc::broken_intra_doc_links,
+//     rustdoc::missing_crate_level_docs,
+//     rustdoc::missing_doc_code_examples
+// )]
+
 pub mod connector;
 pub mod spawn;
 pub mod types;
