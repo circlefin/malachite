d68bd8b6e8bc0f857b4effdbdacedbe715990976
Romain Ruetschi <romain@informal.systems>
2025-02-03 13:44:50 +0100

    Update protos for interop

--- code/crates/starknet/p2p-proto/export-proto.sh
+++ code/crates/starknet/p2p-proto/export-proto.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_PATH="$(dirname "$(realpath "$0")")"
 
-ref="3229754141c445deb4ac24238021864c"
+ref="6eddedd9c77749ca929367fa81b53bc7"
 output="$SCRIPT_PATH/proto"
 
 echo "Exporting proto files from 'buf.build/romac/starknet-p2p:$ref' to '$output'..."
