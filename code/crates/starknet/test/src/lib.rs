33cd3f8fda83ea85a665a41338426c40e580af9b
Anca Zamfir <ancazamfir@users.noreply.github.com>
2025-02-10 11:24:57 -0500

    chore(code): Some WAL entries fail to decode (#840)
    * Add test that shows: failed to fill whole buffer
    
    * Add WAL replay error when unable to decode all entries
    
    * Don't add tag w/o value for skipped timers or empty buf to wal
    
    * Move new test to wal
    
    * Logging cleanup
    
    * Fix missing info in logs
    
    * Add error detail to WalReplayError event
    
    * Add tag to wal only if vote encoding is succesful
    
    * Do not instruct WAL to persist step timeouts
    
    * Move WalReplayError detection to background task
    
    * WAL writes all timeouts it is given, leave choice up to consensus
    
    ---------
    
    Co-authored-by: Romain Ruetschi <romain@informal.systems>

--- code/crates/starknet/test/src/lib.rs
+++ code/crates/starknet/test/src/lib.rs
@@ -510,6 +510,9 @@ async fn run_node<S>(
                         Event::Published(msg) if is_full_node => {
                             panic!("Full node unexpectedly published a consensus message: {msg:?}");
                         }
+                        Event::WalReplayError(e) => {
+                            panic!("WAL replay error: {e}");
+                        }
                         _ => (),
                     }
 
@@ -528,14 +531,12 @@ async fn run_node<S>(
                 info!("Waiting until node reaches height {target_height}");
 
                 'inner: while let Ok(event) = rx_event.recv().await {
-                    let Event::StartedHeight(height) = event else {
-                        continue;
-                    };
+                    if let Event::StartedHeight(height) = &event {
+                        info!("Node started height {height}");
 
-                    info!("Node started height {height}");
-
-                    if height.as_u64() == target_height {
-                        break 'inner;
+                        if height.as_u64() == target_height {
+                            break 'inner;
+                        }
                     }
                 }
             }
