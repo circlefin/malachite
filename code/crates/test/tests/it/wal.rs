46a38e26de2f94ed2549638337ce2d1c02030f6f
Anca Zamfir <zamfiranca@gmail.com>
2025-03-10 14:03:26 +0100

    Temp disable parts-only it tests

--- code/crates/test/tests/it/wal.rs
+++ code/crates/test/tests/it/wal.rs
@@ -105,6 +105,7 @@ async fn proposer_crashes_after_proposing(params: TestParams) {
 }
 
 #[tokio::test]
+#[ignore]
 async fn non_proposer_crashes_after_voting_parts_only() {
     non_proposer_crashes_after_voting(TestParams {
         value_payload: ValuePayload::PartsOnly,
@@ -195,6 +196,7 @@ async fn non_proposer_crashes_after_voting(params: TestParams) {
 }
 
 #[tokio::test]
+#[ignore]
 async fn restart_with_byzantine_proposer_1_request_response_parts_only() {
     byzantine_proposer_crashes_after_proposing_1(TestParams {
         vote_sync_mode: Some(VoteSyncMode::RequestResponse),
@@ -215,6 +217,7 @@ async fn restart_with_byzantine_proposer_1_request_response_proposal_and_parts()
 }
 
 #[tokio::test]
+#[ignore]
 async fn restart_with_byzantine_proposer_1_rebroadcast_parts_only() {
     byzantine_proposer_crashes_after_proposing_1(TestParams {
         vote_sync_mode: Some(VoteSyncMode::Rebroadcast),
@@ -317,6 +320,7 @@ async fn byzantine_proposer_crashes_after_proposing_1(params: TestParams) {
 }
 
 #[tokio::test]
+#[ignore]
 async fn restart_with_byzantine_proposer_2_request_response_parts_only() {
     byzantine_proposer_crashes_after_proposing_2(TestParams {
         vote_sync_mode: Some(VoteSyncMode::RequestResponse),
@@ -337,6 +341,7 @@ async fn restart_with_byzantine_proposer_2_request_response_proposal_and_parts()
 }
 
 #[tokio::test]
+#[ignore]
 async fn restart_with_byzantine_proposer_2_rebroadcast_parts_only() {
     byzantine_proposer_crashes_after_proposing_2(TestParams {
         vote_sync_mode: Some(VoteSyncMode::Rebroadcast),
