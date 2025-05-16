b98e33a8559f8885303e6ad41ded287d9f9fd7f2
Romain Ruetschi <romain@informal.systems>
2025-05-16 10:32:38 +0200

    chore: Remove dependency on `protoc`, use the `protox` crate instead

--- code/crates/test/build.rs
+++ code/crates/test/build.rs
@@ -1,17 +1,17 @@
-use std::io::Result;
-
-fn main() -> Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let protos = &["proto/consensus.proto", "proto/sync.proto"];
 
     for proto in protos {
         println!("cargo:rerun-if-changed={proto}");
     }
 
+    let fds = protox::compile(protos, ["proto"])?;
+
     let mut config = prost_build::Config::new();
     config.enable_type_names();
     config.bytes(["."]);
 
-    config.compile_protos(protos, &["proto"])?;
+    config.compile_fds(fds)?;
 
     Ok(())
 }
