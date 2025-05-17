d75a72c486b9656aa988d5d4bf509185c43dc3e5
Romain Ruetschi <romain@informal.systems>
2025-05-17 12:34:20 +0200

    Gracefully handle removed timeouts

--- code/crates/engine/src/wal/entry.rs
+++ code/crates/engine/src/wal/entry.rs
@@ -121,6 +121,8 @@ fn encode_timeout(tag: u8, timeout: &Timeout, mut buf: impl Write) -> io::Result
         TimeoutKind::Prevote => 2,
         TimeoutKind::Precommit => 3,
 
+        // NOTE: Commit, prevote and precommit time limit timeouts have been removed.
+
         // Consensus will typically not want to store these timeouts in the WAL,
         // but we still need to handle them here.
         TimeoutKind::Rebroadcast => 7,
@@ -141,12 +143,21 @@ fn decode_timeout(mut buf: impl Read) -> io::Result<Timeout> {
         2 => TimeoutKind::Prevote,
         3 => TimeoutKind::Precommit,
 
-        // Commit timeouts have been removed in <https://github.com/informalsystems/malachite/pull/976>,
+        // Commit timeouts have been removed in PR #976,
         // but we still need to handle them here in order to decode old WAL entries.
         4 => {
             return Err(io::Error::new(
                 io::ErrorKind::InvalidData,
                 "commit timeouts are no longer supported, ignoring",
+            ))
+        }
+
+        // Prevote/precommit rebroadcast timeouts have been removed in PR #1037,
+        // but we still need to handle them here in order to decode old WAL entries.
+        5 | 6 => {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "prevote/precommit time limit timeouts are no longer supported, ignoring",
             ))
         }
 
