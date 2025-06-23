302bdac75c5f531e883265278d055b64c0e11687
Romain Ruetschi <romain@informal.systems>
2025-06-23 12:02:32 +0200

    fix(code/network): Properly handle liveness messages with `broadcast` p2p protocol

--- code/crates/network/src/lib.rs
+++ code/crates/network/src/lib.rs
@@ -652,11 +652,13 @@ async fn handle_broadcast_event(
                 message.len()
             );
 
-            let event = Event::ConsensusMessage(
-                channel,
-                PeerId::from_libp2p(&peer_id),
-                Bytes::copy_from_slice(message.as_ref()),
-            );
+            let peer_id = PeerId::from_libp2p(&peer_id);
+
+            let event = if channel == Channel::Liveness {
+                Event::LivenessMessage(channel, peer_id, message)
+            } else {
+                Event::ConsensusMessage(channel, peer_id, message)
+            };
 
             if let Err(e) = tx_event.send(event).await {
                 error!("Error sending message to handle: {e}");
