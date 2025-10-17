56d1d0e939cb089118e6a28f5671abd809d45ceb
Romain Ruetschi <romain.ruetschi@circle.com>
2025-10-17 09:25:49 +0000

    Remove redundant log

--- code/crates/engine/src/consensus.rs
+++ code/crates/engine/src/consensus.rs
@@ -551,13 +551,6 @@ where
                     }
 
                     NetworkEvent::RoundCertificate(from, certificate) => {
-                        info!(
-                            %from,
-                            %certificate.height,
-                            %certificate.round,
-                            number_of_votes = certificate.round_signatures.len(),
-                            "Received round certificate"
-                        );
                         if let Err(e) = self
                             .process_input(
                                 &myself,
